24c69f6bef86b8b6394a5876fcb4870683727b43
Rakibul-98 <rakibul.rupom2001@gmail.com>
2024-11-07 19:11:56 +0600

    problem-1 completed

--- src/problem1.ts
+++ src/problem1.ts
@@ -1,1 +1,16 @@
-console.log("Initializing")
+{
+    // Problem 1 : sum of the numbers of an array
+
+    function sumArray (numbers : number[]) : number {
+        let sum : number = 0;
+        numbers.forEach(num =>{
+            sum = sum + num;
+        })
+        return sum;
+    }
+
+    const result = sumArray([1, 2, 3, 4, 5]);
+    console.log(result);
+
+    //
+}
